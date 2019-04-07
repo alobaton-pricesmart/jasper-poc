63bf8e2f3c0554d44d677cc7ef16f8f387d7bb12
alobaton <alobaton.restrepo@gmail.com>
2019-04-07 16:04:57 -0400

    Initial commit

---
+++ src/main/java/com/co/jasperpoc/JasperPocApplication.java
@@ -0,0 +1,13 @@
+package com.co.jasperpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperPocApplication.class, args);
+	}
+
+}
